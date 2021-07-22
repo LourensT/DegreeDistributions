e4d5b5b3cfdeb5917d5d4630e42e1bed87e84122
LourensT <touwenlourens@gmail.com>
2021-07-22 23:00:34 +0200

    implementation first two

---
+++ DegreeDistributions.py
@@ -0,0 +1,43 @@
+import networkx as nx
+from networkx.classes.function import degree
+
+import numpy as np
+
+def DegreeDistribution(G):
+    pmf = {}
+    degrees = list(G.degree((n for n in G.nodes())).values())
+    n = len(degrees)
+    for v in degrees:
+        if v in pmf:
+            pmf[v] += 1/n
+        else:
+            pmf[v] = 1/n
+    
+    return pmf
+
+
+def SizeBiasedDegreeDistribution(G, validate=False):
+
+    # size of network
+    n = G.number_of_nodes()
+    
+    # setting the pmf
+    pmf = {}
+    for node in G.nodes():
+        
+        # calculate probability of selecting this node
+        adjacent = G.adj[node]
+        prob = (1 / n) * sum([1/G.degree(v) for v in adjacent]) # these probs for all nodes sum to 1
+
+        # update the empirical pmf
+        k = G.degree(node)
+        if k in pmf:
+            pmf[k] += prob
+        else:
+            pmf[k] = prob
+
+    return pmf
+
+def RandomFriendDegreeDistribution(G):
+    pass
+
